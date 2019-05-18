95ff26161f7218e79fe7b1a8cb02de046baaebcf
mloweedgar <mloweedgar0@gmail.com>
2019-05-19 01:18:13 +0300

    feat: display drain data

--- js/custom.js
+++ js/custom.js
@@ -2,15 +2,13 @@ var map = L.map('mapid',).setView([51.505, -0.09], 7);
 
 
 
-var attr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.';
-L.tileLayer("http://tile.stamen.com/toner-background/{z}/{x}/{y}.png", {
-    opacity: 0.1,
-    attribution: attr
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 
 var nexrad = L.tileLayer.wms("http://localhost:8080/geoserver/ows", {
-    layers: 'topp:states',
+    layers: 'cite:mitaro_dar',
     format: 'image/png',
     transparent: true,
 }).addTo(map);
